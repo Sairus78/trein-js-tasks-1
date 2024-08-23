4e51200f683c75eacdc6ca7b5025349098482c15
Sairus78 <83472347+Sairus78@users.noreply.github.com>
2024-08-24 00:17:28 +0300

    1

--- js/task-1.js
+++ js/task-1.js
@@ -86,3 +86,76 @@
 // };
 // const iAmSuper = dersKre();
 // console.log(iAmSuper);
+// const onNight = weekCalories.map(week => {
+//   return week.day + week.calories;
+// });
+// const onNight = weekCalories.map(week => {
+//   if (week.calories > 2500) {
+//     return `${week.calories} is toMatch!`;
+//   }
+//   return week.calories;
+// });
+// console.log(onNight);
+
+// const link = document.querySelector('.link');
+// console.log(link.href); // "https://goit.global"
+// link.href = 'https://neo.goit.global';
+// console.log(link.href); // "https://neo.goit.global"
+
+// const image = document.querySelector('.image');
+// console.log(image.src); // https://picsum.photos/id/9/320/240
+// console.log(image.alt); // A laptop
+
+// // image.src = 'https://picsum.photos/id/13/640/480';
+// // image.alt = 'River bank';
+
+// image.src =
+//   'https://images.pexels.com/photos/26924196/pexels-photo-26924196.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+// image.alt = 'elephant';
+// console.log(image.alt);
+
+// const el = document.querySelector('.text');
+// const nested = document.querySelector('.sub-text');
+
+// console.log(el.textContent); // "Username: Mango"
+// console.log(nested.textContent); // "Mango"
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// // Класичний for
+// for (let i = 0; i < numbers.length; i += 1) {
+//   console.log(`Index ${i}, value ${numbers[i]}`);
+// }
+
+// // Перебираючий метод forEach
+// numbers.forEach(function (number, index) {
+//   console.log(`Index ${index}, value ${number}`);
+// });
+
+const books = [
+  { title: 'To Kill a Mockingbird', author: 'Harper Lee' },
+  { title: '1984', author: 'George Orwell' },
+  { title: 'Moby Dick', author: 'Herman Melville' },
+  { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+  { title: 'Pride and Prejudice', author: 'Jane Austen' },
+];
+const getBook = document.querySelector('.book-list');
+
+const fragment = document.createDocumentFragment();
+
+books.forEach(book => {
+  const itemEl = document.createElement('li');
+
+  const spanTitle = document.createElement('span');
+  spanTitle.classList.add('book-title');
+
+  const spanAutor = document.createElement('span');
+  spanAutor.classList.add('book-author');
+
+  spanTitle.textContent = book.title;
+  spanAutor.textContent = book.author;
+
+  itemEl.append(spanTitle, ' by ', spanAutor);
+  fragment.append(itemEl);
+});
+getBook.append(fragment);
